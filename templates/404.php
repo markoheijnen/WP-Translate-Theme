31badb62aaab1e83d4532ad3318ccdbf236f2310
Marko Heijnen <info@markoheijnen.com>
2014-01-13 00:51:32 +0100

    Design the 404 a bit more. Also adds missing fonts. Fixes #1

--- templates/404.php
+++ templates/404.php
@@ -3,7 +3,11 @@ gp_title( __('Not Found &lt; GlotPress') );
 gp_tmpl_header();
 ?>
 
-		<h2><?php _e('Not found'); ?></h2>
+		<h2><?php _e('Page not found'); ?></h2>
+
+		<p>The page you requested could not be found.</p>
+
+		<a href="<?php echo gp_url( '/' ); ?>" class="btn btn-large btn-primary"><span class="glyphicon glyphicon-home"></span> Take Me Home</a>
 
 <?php
 gp_tmpl_footer();
