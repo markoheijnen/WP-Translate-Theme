354dbe642542198dd3535d48bab6241de4bc3357
Marko Heijnen <info@markoheijnen.com>
2014-01-13 15:41:32 +0100

    Add projects overview template. Fixes #9

--- templates/projects.php
+++ templates/projects.php
@@ -3,11 +3,14 @@ gp_title( __('Projects &lt; GlotPress') );
 gp_tmpl_header();
 ?>
 
-		<h2><?php printf( __('Projects')); ?></h2>
+		<h2><?php printf( __('Projects') ); ?></h2>
 
-		<ul>
+		<ul class="list-group">
 		<?php foreach($projects as $project): ?>
-			<li><?php gp_link_project( $project, esc_html( $project->name ) ); ?> <?php gp_link_project_edit( $project, null, array('class' => 'bubble') ); ?></li>
+			<li class="list-group-item">
+				<?php gp_link_project( $project, esc_html( $project->name ) ); ?>
+				<?php gp_link_project_edit( $project, null, array( 'class' => 'btn btn-xs btn-primary' ) ); ?>
+			</li>
 		<?php endforeach; ?>
 		</ul>
 
