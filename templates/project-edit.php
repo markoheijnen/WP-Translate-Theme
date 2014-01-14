713bf40f3990a3bd3e75c46a7c0fb1ea48175c5c
Marko Heijnen <info@markoheijnen.com>
2014-01-14 23:47:14 +0100

    Align the buttons for new and edit project form better. See #3 and #7

--- templates/project-edit.php
+++ templates/project-edit.php
@@ -9,10 +9,13 @@ gp_tmpl_header();
 		<form action="" method="post" class="form-left form-horizontal" role="form">
 			<?php gp_tmpl_load( 'project-form', get_defined_vars()); ?>
 
-			<p>
-				<input type="submit" name="submit" value="<?php echo esc_attr( __('Save') ); ?>" id="submit" class="btn btn-primary" />
-				<span class="or-cancel"><?php _e('or'); ?> <a href="javascript:history.back();"><?php _e('Cancel'); ?></a></span>
-			</p>
+			<div class="form-group">
+				<div class="col-sm-offset-4 col-md-offset-3 col-sm-8">
+					<input type="submit" name="submit" value="<?php echo esc_attr( __('Save') ); ?>" id="submit" class="btn btn-primary" />
+					<span class="or-cancel"><?php _e('or'); ?> <a href="javascript:history.back();"><?php _e('Cancel'); ?></a></span>
+				</div>
+			</div>
+
 		</form>
 
 <?php gp_tmpl_footer();
