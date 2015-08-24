29315582b8d3d58b1f57e07a289897339c551730
Marko Heijnen <info@markoheijnen.com>
2015-08-24 23:28:20 +0200

    Cleanup old mess in profile template. Fixes #25

--- templates/profile.php
+++ templates/profile.php
@@ -9,60 +9,8 @@ if ( 0 == $per_page ) {
 
 $default_sort = GP::$user->current()->sort_defaults();
 
-if ( GP_Bootstrap_Theme::has_feature('profile') ) {
-	wp_enqueue_script( 'goog-jsapi' ); 
-	wp_enqueue_script( 'goog-charts' ); 
-	wp_localize_script( 'goog-charts', 'days', $dates ); 
-}
-
 gp_tmpl_header();
 ?>
-
-		<h2><?php _e( "Profile" ); ?></h2>
-
-		<?php if ( GP_Bootstrap_Theme::has_feature('profile') ) { ?>
-		<div id="profile">
-			<div id="statistics">
-				<h3>Statistics: </h3>
-				<span><b><?php echo $total_strings; ?></b> <?php _e( "Strings Translated" ); ?></span>
-				<span><b><?php echo $project_contrib_count; ?></b> <?php _e( "Projects Contributed to" ); ?></span>
-				<div id="week-progress"></div>
-			</div>
-			<div id="project-shortcuts">
-				<h3>Project Shortcuts:</h3>
-				<table class="translation-sets proj-shortcuts">
-					<thead>
-					<tr>
-						<th class="">Sub Project</th>
-						<th class="header">Set</th>
-					</tr>
-					</thead>
-					<tbody>
-					<?php foreach( $shortcuts as $shortcut ): ?>
-						<tr>
-							<th><?php echo $shortcut['project_path']; ?><?php if ( $shortcut['active'] ) echo "<span class='active bubble'>" . __('Active') . "</span>"; ?></th>
-							<td>
-								<strong><?php gp_link( $shortcut['link'], $shortcut['contrib']->name_with_locale() ); ?></strong>
-								<span> &#45; <?php echo $shortcut['contrib']->current_perc ?></span>
-								<div class="progress-bar"
-									 title="
-									 <?php echo sprintf("Waiting: %s, Translated: %s, Fuzzy: %s",
-										 $shortcut['contrib']->waiting_perc,
-										 $shortcut['contrib']->current_perc,
-										 $shortcut['contrib']->fuzzy_perc) ;?>">
-									<span class="words-reviewed-perc" style="width:<?php echo $shortcut['contrib']->current_perc; ?>%"></span><!--
-								 --><span class="words-waiting-perc" style="width:<?php echo $shortcut['contrib']->waiting_perc; ?>%"></span>
-								</div>
-							</td>
-						</tr>
-					<?php endforeach; ?>
-					</tbody>
-				</table>
-			</div>
-		</div>
-
-		<h3>Settings</h3>
-		<?php } ?>
 
 		<form action="" method="post" class="form-left form-horizontal" role="form">
 			<div class="form-group">
