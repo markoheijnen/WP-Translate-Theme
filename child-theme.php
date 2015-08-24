eda6c91c552a8c6f55245ef563607b1890f68c98
Marko Heijnen <info@markoheijnen.com>
2015-08-24 14:42:31 +0200

    Fix spelling of GlotPress in install script.

--- child-theme.php
+++ child-theme.php
@@ -6,7 +6,7 @@ class GP_Bootstrap_Theme extends GP_Plugin {
 	public $id                = 'bootstrap_theme';
 	const version             = '1.0';
 	const bootstrap_version   = '3.2.0';
-	const glotpress_changeset = '1056';
+	const glotpress_changeset = '1057';
 
 	private $child_path;
 
